d036cd2a3c49883395cb890624d782329ff0fba1
jrjawad <jrjawad@gmail.com>
2024-11-14 15:14:17 +0500

    Initial commit

---
+++ DotNet_Jenkins/DotNet_Jenkins/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNet_Jenkins.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
